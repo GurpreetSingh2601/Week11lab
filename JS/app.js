2a09d071c24658fa37e28d605914012021ad0048
Gurpreet Singh <gurubahre2019@gmail.com>
2021-03-26 16:30:59 -0700

    Adding for loop in JS file

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,12 @@
-// your JavaScript goes here, you can delete this comment
+let todolist = [{ todo: "clone repo for starter code", status: "complete" }, { todo: "release proper commoit messages at regular intervals", status: "started" }]
+completedtodos = []
+
+for (i = 0; i < todolist.length; i++) {
+
+    if (todolist[i].status === "complete") {
+
+        completedtodos.push(todolist[i])
+
+    }
+}
+console.log(completedtodos)
